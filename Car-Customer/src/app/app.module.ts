e367df11381454b02a0e3a71b4d50dc77de36b28
jayesh dhandha <jayudhandha@gmail.com>
2023-03-25 17:28:18 +0530

    added material you

--- Car-Customer/src/app/app.module.ts
+++ Car-Customer/src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialAppModule } from '../material.module';
 
 @NgModule({
   declarations: [AppComponent, LoginPageComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialAppModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
